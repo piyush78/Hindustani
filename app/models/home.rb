7d8991f980b8f51c47f652fe6cbe85cad5bbd616
piyush78 <peyushchowhan7@gmail.com>
2018-10-12 15:54:01 +0530

    added vaildation

--- app/models/home.rb
+++ app/models/home.rb
@@ -2,6 +2,7 @@ class Home < ApplicationRecord
   has_many :home
    has_one :father
    has_one :mother
+   validates :Name, :Aadhar, :Dob, :Father, :Mother, :Children, presence: true
 
   #attr_reader :Name, :Aadhar, :Dob, :Father, :Mother, :Children
 end
